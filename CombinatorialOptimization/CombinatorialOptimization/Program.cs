0b8ab82ea036c7ba4c5fdd8c68de628593f813ff
cafeunder <cafe3641@gmail.com>
2017-05-19 03:59:43 +0900

    C#に変えてやり直す

---
+++ CombinatorialOptimization/CombinatorialOptimization/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombinatorialOptimization {
+	class Program {
+		static void Main(string[] args) {
+			int a = 0;
+			int d = a;
+		}
+	}
+}
